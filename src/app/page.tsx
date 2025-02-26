396158281838fd4ee73dc160e87b79adc11d1447
Jacek <jacek@degen.tips>
2025-02-26 17:48:30 +0100

    Update title metadata.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const frame = {
   version: 'next',
   imageUrl: `${appUrl}/image.gif`,
   button: {
-    title: 'Launch Frame',
+    title: 'Start',
     action: {
       type: 'launch_frame',
       name: 'Degen Official',
