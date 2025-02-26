af7b8db0a43694d1c2fff97ba206a1982686b822
Jacek <jacek@degen.tips>
2025-02-26 15:15:10 +0100

    Create Footer component.

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="mt-6 text-center text-xs text-white">
+      <p>Come back every day for fresh surprises, and meme magic!</p>
+      <p className="italic mt-2 text-yellow-200">Stay meme, stay degen!</p>
+    </footer>
+  );
+}
