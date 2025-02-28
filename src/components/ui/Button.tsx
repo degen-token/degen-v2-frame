40ffec364ddbb2b1109476fdffa62591ce389273
Jacek <jacek@degen.tips>
2025-02-28 01:53:29 +0100

    Update styling.

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type="button"
       onClick={onClick}
-      className={`bg-violet-500 px-3.5 py-2.5 text-sm font-semibold text-slate-900 shadow-sm hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 ${className}`}
+      className={`bg-violet-500 px-3.5 py-2.5 font-semibold text-slate-900 shadow-sm hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 ${className}`}
     >
       {children}
     </button>
