12aa311bebdd78b8c296b1da2ff559146b95df47
Jacek <jacek@degen.tips>
2025-03-01 13:41:26 +0100

    Use clsx in code.

--- src/components/ui/ButtonPrimary.tsx
+++ src/components/ui/ButtonPrimary.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface ButtonProps {
@@ -15,7 +16,10 @@ export default function ButtonPrimary({
     <button
       type="button"
       onClick={onClick}
-      className={`bg-violet-500 px-3.5 py-2.5 font-semibold text-slate-900 shadow-sm hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 ${className}`}
+      className={clsx(
+        'bg-violet-500 px-3.5 py-2.5 font-semibold text-slate-900 shadow-sm hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500',
+        className
+      )}
     >
       {children}
     </button>
