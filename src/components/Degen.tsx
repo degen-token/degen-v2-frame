999532d6bc94fc560c5f35e9bf97cd43975d1284
Jacek <jacek@degen.tips>
2025-03-05 15:08:08 +0100

    Organize imports.

--- src/components/Degen.tsx
+++ src/components/Degen.tsx
@@ -1,16 +1,14 @@
 import sdk, { type Context } from '@farcaster/frame-sdk';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 
-import { Button } from '@/components/catalyst/button';
 import { Heading } from '@/components/catalyst/heading';
-import { config } from '@/components/providers/WagmiProvider';
+import AirdropClaimButton from '@/components/ui/AirdropClaimButton';
 import Container from '@/components/ui/Container';
 import Footer from '@/components/ui/Footer';
-import AirdropClaimButton from './ui/AirdropClaimButton';
+import ConnectWalletButton from './ui/ConnectWalletButton';
 
 export default function Degen() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
@@ -45,30 +43,7 @@ export default function Degen() {
     <Container>
       <div className="absolute top-4 right-4">
         {/* Wallet Button */}
-        <Button
-          outline
-          onClick={() =>
-            isConnected
-              ? disconnect()
-              : connect({ connector: config.connectors[1] })
-          }
-          className="flex items-center space-x-2 text-xs px-2 py-1 h-8"
-        >
-          {isConnected ? (
-            <>
-              <Image
-                src={context?.user.pfpUrl || '/default_pfp.jpg'}
-                alt="Degen Profile"
-                width={120}
-                height={120}
-                className="w-6 h-6 mr-2 rounded-xs border-2 border-slate-800/30 shadow-lg"
-              />
-              {address?.slice(0, 6)}...{address?.slice(-4)}
-            </>
-          ) : (
-            <>Connect</>
-          )}
-        </Button>
+        <ConnectWalletButton />
       </div>
 
       {showConfetti && <Confetti numberOfPieces={500} recycle={true} />}
