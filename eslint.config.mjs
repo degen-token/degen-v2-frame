028fc0617a338d03fd5f2ef3c4dafcbde4899bcf
Jacek <jacek@degen.tips>
2025-03-05 15:53:20 +0100

    Switch to warn for eslint rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      'prefer-const': 'off',
+      'prefer-const': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
