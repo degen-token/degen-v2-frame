b8331ad1a8f2c58d42e79acf35c07b0cd5042ee4
Jacek <jacek@degen.tips>
2025-03-05 14:43:52 +0100

    Update button outline to fit design.

--- src/components/catalyst/button.tsx
+++ src/components/catalyst/button.tsx
@@ -44,7 +44,7 @@ const styles = {
     // Base
     'border-zinc-950/10 text-zinc-950 data-active:bg-zinc-950/[2.5%] data-hover:bg-zinc-950/[2.5%]',
     // Dark mode
-    'dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-active:bg-white/5 dark:data-hover:bg-white/5',
+    'dark:border-violet-500/50 dark:text-white dark:[--btn-bg:transparent] dark:data-active:bg-white/5 dark:data-hover:bg-white/5',
     // Icon
     '[--btn-icon:var(--color-zinc-500)] data-active:[--btn-icon:var(--color-zinc-700)] data-hover:[--btn-icon:var(--color-zinc-700)] dark:data-active:[--btn-icon:var(--color-zinc-400)] dark:data-hover:[--btn-icon:var(--color-zinc-400)]',
   ],
