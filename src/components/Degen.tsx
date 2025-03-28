369b43f3d9e81bc597bb546aad874d85277f57de
Jacek <jacek@degen.tips>
2025-03-28 13:25:02 +0100

    Add airdrop 2 season 14 rewards.

--- src/components/Degen.tsx
+++ src/components/Degen.tsx
@@ -37,12 +37,11 @@ export default function Degen() {
         whileHover={{ scale: 1.05 }}
       >
         <Heading>Airdrop</Heading>
-        <Subheading className="animate-pulse">Season 13</Subheading>
+        <Subheading className="animate-pulse">Season 14</Subheading>
 
         <AirdropClaimButton
           airdropContract="41a76c8FEA906Dc11A825E3B1a3Fb9deef5B7521"
           merkleProofApi="https://api.degen.tips/airdrop2/season14/merkleproofs"
-          isDegenChain={false}
         />
       </motion.div>
     </Container>
